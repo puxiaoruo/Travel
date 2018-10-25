ae693f440e3f26a06458383c189db2843f69d49a
puxiaoruo <1366928318@qq.com>
2018-10-25 17:01:18 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
+import 'swiper/dist/css/swiper.css'
 import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item in swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl" alt="">
+        </swiper-slide>
+        <!-- <swiper-slide>
+            <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg" alt="">
+        </swiper-slide> -->
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    </div>
+</template>
+<script>
+export default {
+    name: "HomeSwiper",
+    data() {
+      return {
+        swiperOption: {
+          pagination: ".swiper-pagination",
+          loop: true,
+        },
+        swiperList: [
+            {
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+            },
+            {
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+            }
+        ]
+      }
+    },
+}
+</script>
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background-color :#fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        .swiper-img
+            width : 100%
+</style>
