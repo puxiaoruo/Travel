7802997cd84056826030d58dc921e86df88ed74c
puxiaoruo <1366928318@qq.com>
2018-11-13 17:38:19 +0800

    完善搜索

--- src/pages/list/components/search.vue
+++ src/pages/list/components/search.vue
@@ -1,12 +1,60 @@
 <template>
-    <div class="search">
-       <input type="text" class="search-input" placeholder="输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input type="text" v-model="keyworld" class="search-input" placeholder="输入城市名或拼音">
+        </div>
+        <div class="search-content" ref="search" v-if="keyworld != ''">
+            <ul>
+                <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+                    {{item.name}}
+                </li>
+                <li class="search-item border-bottom" v-show="!list.length">
+                    没有找到行管数据
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
     name: 'CitySearch',
+    props: {
+        cities : Object
+    },
+    data () {
+        return {
+            keyworld: "",
+            list: [],
+            timer: null
+        }
+    },
+    watch: {
+        keyworld() {
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+            if(this.keyworld == ""){
+                this.list = [];
+                return 
+            }
+            this.timer = setTimeout( () => {
+                const result = [];
+                for(let i in this.cities){
+                    this.cities[i].forEach((value) => {
+                        if(value.spell.indexOf(this.keyworld) > -1 || value.name.indexOf(this.keyworld) > -1){
+                            result.push(value);
+                        }
+                    });
+                }
+                this.list = result
+            } ,16)
+        }
+    },
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.search)
+    }
 }
 </script>
 <style lang="stylus" scoped>
@@ -26,5 +74,18 @@ export default {
             border-radius :.06rem;
             border: none;
             outline : none  
+    .search-content
+        overflow hidden;
+        position absolute;
+        top: 1.58rem;
+        left : 0;
+        right : 0;
+        bottom : 0;
+        background-color #eee;
+        z-index : 1
+        .search-item
+            line-height .62rem;
+            padding-left .2rem;
+            background-color #fff;
     
 </style>
